3092fe3f103039950bd494ec3151e521ae784386
mprussiani <mprussiani@localhost>
2018-02-20 15:33:52 +0100

    upd

---
+++ src/main/java/com/sorint/primefaces/model/HelloWorld.java
@@ -0,0 +1,49 @@
+package com.sorint.primefaces.model;
+
+import javax.faces.bean.ManagedBean;
+
+import com.sorint.primefaces.utility.ServiceIp;
+
+//import com.sorint.primefaces.utility.ServiceIp;
+
+@ManagedBean
+public class HelloWorld {
+
+	private String firstName = "Mario";
+	private String lastName = "Rossi";
+	private String ip = "";
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String showGreeting() {
+		return "Ciao " + firstName + " " + lastName;
+	}
+
+	public String getIp1() {
+		ServiceIp serviceIp = new ServiceIp();
+		return serviceIp.ip();
+	}
+
+	public String getIp() {
+		return ip+"IP: "+getIp1();
+	}
+
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+
+}
